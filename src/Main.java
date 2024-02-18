f4aa5c213bc871019f0b7a24918aa0150e2dceb6
User <xtoycohcohu@mail.ru>
2024-02-18 12:26:55 +0700

    Я во втором задании сделала по-своему. Так же можно было? Уменя просто точность меньше. Верно?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
     }
-}
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i <= arr.length-1; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        int[] arr = generateRandomArray();
+        int maxValue = -1;
+        int min = maxValue;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (maxValue < arr[i]) {
+                maxValue = arr[i];
+            }
+            min = maxValue;
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Максимальная трата за день составила " + maxValue + " рублей");
+        System.out.println("Минимальная трата за день составила " + min + " рублей");
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        int[] arr = generateRandomArray();
+        long sum = 0;
+        double averageValue = 0;
+        for (int i = 0; i <= arr.length-1; i++) {
+            sum = sum + arr[i];
+            averageValue = (double) sum / arr.length;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + averageValue + " рублей");
+    }
+    public static void task4() {
+        System.out.println("Задание 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length-1; i >= 0; i--){
+                    System.out.print(reverseFullName[i]);
+                }
+            }
+    }
+
+
